c107b9287e19ae258f620fb2fb75bd39d4315d36
Xiao Liwen <242320030@hdu.edu.cn>
2025-05-02 23:15:50 +0800

    first commit

---
+++ build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_Xchat_FILE D:/Qt/Project/Xchat/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/Xchat.exe)
+set(__QT_DEPLOY_TARGET_Xchat_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_Xchat_RUNTIME_DLLS D:/Qt/6.8.0/mingw_64/bin/Qt6Widgets.dll;D:/Qt/6.8.0/mingw_64/bin/Qt6Gui.dll;D:/Qt/6.8.0/mingw_64/bin/Qt6Core.dll)
